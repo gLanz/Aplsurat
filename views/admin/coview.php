d1cc11b6ac8110bf2b5162044d494452da7cd29f
Faber Nainggolan <goeriks@gmail.com>
2019-03-02 17:20:54 +0700

    Apl Surat 1.2

--- views/admin/coview.php
+++ views/admin/coview.php
@@ -1,4 +1,4 @@
-<? 
+<?php 
 require"../../config/includes.php";
 ?>
 <script type="text/javascript">
@@ -13,7 +13,7 @@ require"../../config/includes.php";
 		});
 	});
 </script>
-<? if($_REQUEST['det']=='1')
+<?php if($_REQUEST['det']=='1')
 {
 	$id = $_GET['id'];
 	$content=new Surat();
@@ -31,31 +31,31 @@ require"../../config/includes.php";
  <table width="664" class="">
    <tr>
      <td width="31%" class="td3">Nomor Surat</td>
-     <td width="66%" class="td4"><?=$record[nosurat_su]?></td>
+     <td width="66%" class="td4"><?=$record['nosurat_su']?></td>
    </tr>
    <tr >
      <td class="td3">Perihal </td>
-     <td class="td4"><?=$record[perihal_su]?></td>
+     <td class="td4"><?=$record['perihal_su']?></td>
    </tr>
    <tr>
      <td class="td3">Asal Surat</td>
-     <td class="td4"><?=$record[asal_su]?></td>
+     <td class="td4"><?=$record['asal_su']?></td>
    </tr>
    <tr>
      <td class="td3">Tujuan</td>
-     <td class="td4"><?=$record[tujuan_su]?></td>
+     <td class="td4"><?=$record['tujuan_su']?></td>
    </tr>
    <tr>
      <td class="td3">Penerima</td>
-     <td class="td4"><?=$record[penerima_su]?></td>
+     <td class="td4"><?=$record['penerima_su']?></td>
    </tr>
    <tr>
      <td class="td3">Tangal Surat</td>
-     <td class="td4"><?=ubahTanggal($record[tglmasuk_su])?></td>
+     <td class="td4"><?=ubahTanggal($record['tglmasuk_su'])?></td>
    </tr>
    <tr>
      <td class="td3">Nomor Agenda</td>
-     <td class="td4"><?=$record[no_agendasurat]?></td>
+     <td class="td4"><?=$record['no_agendasurat']?></td>
    </tr>
  </table>
     <div id="untukTombol"><span style="float:left;">Tanggal Hari Ini : <i><?=ubahTanggal(date("Y-m-d"));?></i></span>
@@ -67,7 +67,7 @@ require"../../config/includes.php";
 			  <div class="submit_link">
             </div>
          </footer>
-<? }elseif($_REQUEST['det']=='2')
+<?php }elseif($_REQUEST['det']=='2')
 {	$id = $_GET['id'];
 	$content=new Surat();
 	$pegRecordSet = $content->getSuratSessionById($id); // get the record set for this Id.
@@ -84,31 +84,31 @@ require"../../config/includes.php";
  <table width="664" class="">
    <tr>
      <td width="31%" class="td3">Nomor Surat</td>
-     <td width="66%" class="td4"><?=$record[nosurat_su]?></td>
+     <td width="66%" class="td4"><?=$record['nosurat_su']?></td>
    </tr>
    <tr >
      <td class="td3">Perihal </td>
-     <td class="td4"><?=$record[perihal_su]?></td>
+     <td class="td4"><?=$record['perihal_su']?></td>
    </tr>
    <tr>
      <td class="td3">Asal Surat</td>
-     <td class="td4"><?=$record[asal_su]?></td>
+     <td class="td4"><?=$record['asal_su']?></td>
    </tr>
    <tr>
      <td class="td3">Tujuan</td>
-     <td class="td4"><?=$record[tujuan_su]?></td>
+     <td class="td4"><?=$record['tujuan_su']?></td>
    </tr>
    <tr>
      <td class="td3">Penerima</td>
-     <td class="td4"><?=$record[penerima_su]?></td>
+     <td class="td4"><?=$record['penerima_su']?></td>
    </tr>
    <tr>
      <td class="td3">Tangal Surat</td>
-     <td class="td4"><?=ubahTanggal($record[tglmasuk_su])?></td>
+     <td class="td4"><?=ubahTanggal($record['tglmasuk_su'])?></td>
    </tr>
    <tr>
      <td class="td3">Nomor Agenda</td>
-     <td class="td4"><?=$record[no_agendasurat]?></td>
+     <td class="td4"><?=$record['no_agendasurat']?></td>
    </tr>
   
   <tr>
@@ -117,23 +117,23 @@ require"../../config/includes.php";
    </tr>
   <tr>
     <td class="td3">Tanggal diterima</td>
-    <td class="td4"><?=ubahTanggal($record[tglditerima])?></td>
+    <td class="td4"><?=ubahTanggal($record['tglditerima'])?></td>
   </tr>
   <tr>
     <td class="td3">Tanggal disposisi</td>
-    <td class="td4"><?=ubahTanggal($record[tgldisposisi])?></td>
+    <td class="td4"><?=ubahTanggal($record['tgldisposisi'])?></td>
   </tr>
   <tr>
     <td class="td3">Isi Disposisi</td>
-    <td class="td4"><?=$record[isidisposisi]?></td>
+    <td class="td4"><?=$record['isidisposisi']?></td>
   </tr>
   <tr>
     <td class="td3">Pendisposisi</td>
-    <td class="td4"><?=$record[pendisposisi]?></td>
+    <td class="td4"><?=$record['pendisposisi']?></td>
   </tr>
   <tr>
     <td class="td3">Tindak lanjut</td>
-    <td class="td4"><?=$record[tindaklanjut]?></td>
+    <td class="td4"><?=$record['tindaklanjut']?></td>
   </tr> 
  </table>
     <div id="untukTombol"><span style="float:left;">Tanggal Hari Ini : <i><?=ubahTanggal(date("Y-m-d"));?></i></span>
@@ -145,7 +145,7 @@ require"../../config/includes.php";
 			  <div class="submit_link">
             </div>
 </footer>
-<? }elseif($_REQUEST['det']=='3')
+<?php }elseif($_REQUEST['det']=='3')
 {
 	$id = $_GET['id'];
 	$content=new Surat();
@@ -163,51 +163,51 @@ require"../../config/includes.php";
  <table width="664" class="">
    <tr>
      <td width="31%" class="td3">Nomor Agenda</td>
-     <td width="66%" class="td4"><?=$record[sk_noagenda]?></td>
+     <td width="66%" class="td4"><?=$record['sk_noagenda']?></td>
    </tr>
    <tr >
      <td class="td3">Perihal </td>
-     <td class="td4"><?=$record[sk_perihal]?></td>
+     <td class="td4"><?=$record['sk_perihal']?></td>
    </tr>
    <tr>
      <td class="td3">Tanggal Surat Keluar</td>
-     <td class="td4"><?=ubahTanggal($record[sk_tglkeluar])?></td>
+     <td class="td4"><?=ubahTanggal($record['sk_tglkeluar'])?></td>
    </tr>
    <tr>
      <td class="td3">Pengirim</td>
-     <td class="td4"><?=$record[sk_pengirim]?></td>
+     <td class="td4"><?=$record['sk_pengirim']?></td>
    </tr>
    <tr>
      <td class="td3">Tanggal Surat</td>
-     <td class="td4"><?=ubahTanggal($record[sk_tglkeluar])?></td>
+     <td class="td4"><?=ubahTanggal($record['sk_tglkeluar'])?></td>
    </tr>
    <tr>
      <td class="td3">Yang Menyerahkan Surat</td>
-     <td class="td4"><?=$record[sk_penyerah]?></td>
+     <td class="td4"><?=$record['sk_penyerah']?></td>
    </tr>
    <tr>
      <td class="td3">Yang Menerima Surat</td>
-     <td class="td4"><?=$record[sk_penerima]?></td>
+     <td class="td4"><?=$record['sk_penerima']?></td>
    </tr>
    <tr>
      <td class="td3">Tangal Terima</td>
-     <td class="td4"><?=ubahTanggal($record[sk_tglterima])?></td>
+     <td class="td4"><?=ubahTanggal($record['sk_tglterima'])?></td>
    </tr>
    <tr>
      <td class="td3">Tanggal Kirim</td>
-     <td class="td4"><?=ubahTanggal($record[sk_tglkirim])?></td>
+     <td class="td4"><?=ubahTanggal($record['sk_tglkirim'])?></td>
    </tr>
       <tr>
      <td class="td3">Jenis Pengiriman</td>
-     <td class="td4"><?=$record[sk_jeniskirim]?></td>
+     <td class="td4"><?=$record['sk_jeniskirim']?></td>
    </tr>
       <tr>
      <td class="td3">Nomor Resi Pengiriman</td>
-     <td class="td4"><?=$record[sk_noresi]?></td>
+     <td class="td4"><?=$record['sk_noresi']?></td>
    </tr>
       <tr>
      <td class="td3">Tanggal Resi</td>
-     <td class="td4"><?=ubahTanggal($record[sk_tglresi])?></td>
+     <td class="td4"><?=ubahTanggal($record['sk_tglresi'])?></td>
    </tr>
  </table>
     <div id="untukTombol"><span style="float:left;">Tanggal Hari Ini : <i><?=ubahTanggal(date("Y-m-d"));?></i></span>
@@ -219,7 +219,7 @@ require"../../config/includes.php";
 			  <div class="submit_link">
             </div>
          </footer>
-<? }elseif($_REQUEST['det']=='4'){
+<?php }elseif($_REQUEST['det']=='4'){
 $id = $_GET['id'];
 	$content=new Questbook();
 	$pegRecordSet = $content->getQuestById($id); // get the record set for this Id.
@@ -238,19 +238,19 @@ $id = $_GET['id'];
 <table width="70%" align="left" class="tableinput" style="border-collapse:collapse;">
   <tr>
     <td class="td3"><div align="left">Id</div></td>
-    <td width="58%" class="td2"><?=$record[idquest]?></td>
+    <td width="58%" class="td2"><?=$record['idquest']?></td>
   </tr>
     <tr>
     <td class="td3"><div align="left">Nama </div></td>
-    <td class="td2"><?=$record[nama]?></td>
+    <td class="td2"><?=$record['nama']?></td>
   </tr>
   <tr>
     <td class="td3"><div align="left">Email</div></td>
-    <td class="td2"><?=$record[email]?></td>
+    <td class="td2"><?=$record['email']?></td>
   </tr>
   <tr>
     <td width="22%" class="td3"><div align="left">Isi</div></td>
-    <td valign="top" class="td2"><?=$record[komentar]?></td>
+    <td valign="top" class="td2"><?=$record['komentar']?></td>
   </tr>
     <tr>
       <td height="25" colspan="2" align="right" valign="top" class="td3">
@@ -261,13 +261,13 @@ $id = $_GET['id'];
     <tr>
       <td width="21%" valign="top" class="td2">Header</td>
       <td width="79%" class="td2">Subject : [
-        Re <?=substr($record[komentar],0,50)?>...]<br />
+        Re <?=substr($record['komentar'],0,50)?>...]<br />
         From : [ <?=$client_email?> ]<br />
         Text : [ text email ]<br />
         Html : [ Balasan Testimonial/Komentar ]<br />
         Send to : [
         <?=$record[3]?> <input type="hidden" value="<?=$record[3]?>" name="email" />
-        <input type="hidden" value="<?=substr($record[pesan],0,50)?>" name="subjek" />
+        <input type="hidden" value="<?=substr($record['pesan'],0,50)?>" name="subjek" />
         <input type="hidden" value="<?=$record[0]?>" name="japid" />
         ]</td>
       </tr>
@@ -291,4 +291,4 @@ $id = $_GET['id'];
 			  <div class="submit_link">
             </div>
          </footer>
-<? }?>
+<?php }?>
